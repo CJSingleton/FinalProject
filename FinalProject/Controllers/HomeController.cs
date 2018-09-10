b8f55720afa4f44c03d25ed3490203a973582210
Christopher <cjsing11@gmail.com>
2018-09-10 13:35:42 -0400

    first commit for FinalProject

---
+++ FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {   ////api.census.gov/data/2016/acs/acs1?get=NAME,B01001_001E&for=state:26                            
+            //api.census.gov/data/2016/acs/acs1/profile?get=DP02_0001PE&for=state:*
+            HttpWebRequest apiRequest = WebRequest.CreateHttp("https://api.census.gov/data/2016/acs/acs1/profile?get=NAME,DP02_0001E,DP02_0002E,DP02_0003E&for=state:26&for=cd115:*");
+            apiRequest.Headers.Add("X-Census-Key", ConfigurationManager.AppSettings["X-Census-Key"]); // used to add keys.
+            apiRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+            HttpWebResponse apiResponse = (HttpWebResponse)apiRequest.GetResponse();
+
+            if (apiResponse.StatusCode == HttpStatusCode.OK) // (== 200) if we get status of 200, things are good.
+            {
+                StreamReader responseData = new StreamReader(apiResponse.GetResponseStream());// use System.IO
+                string data = responseData.ReadToEnd(); //reads data from the response
+
+                JArray jsonData = JArray.Parse(data);
+                
+                ViewBag.test1 = jsonData/*[1]*/;
+                //ViewBag.triviadate = jsonCensusData["year"];
+
+            }
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Questions()
+        {
+            ViewBag.Message = "Your form page.";
+
+            return View();
+        }
+        public ActionResult frontendtemp()
+        {
+            return View();
+        }
+    }
+}
