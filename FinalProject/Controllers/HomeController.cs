13bfb2b04bec2a53dcd02da2635673024dab203f
Christopher <cjsing11@gmail.com>
2018-09-12 12:03:08 -0400

    page for retrieving user data

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -7,15 +7,15 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Configuration;
+using FinalProject.Models;
 
 namespace FinalProject.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {   ////api.census.gov/data/2016/acs/acs1?get=NAME,B01001_001E&for=state:26                            
-            //api.census.gov/data/2016/acs/acs1/profile?get=DP02_0001PE&for=state:*
-            HttpWebRequest apiRequest = WebRequest.CreateHttp("https://api.census.gov/data/2016/acs/acs1/profile?get=NAME,DP02_0001E,DP02_0002E,DP02_0003E&for=state:26&for=cd115:*");
+        {
+            HttpWebRequest apiRequest = WebRequest.CreateHttp("https://api.census.gov/data/2016/acs/acs1/profile?get=NAME,DP05_0009E,DP05_0002E,DP02_0004E,DP02_0005E,DP03_0004E,DP03_0057E,DP04_0096E,DP04_0129E&for=county+subdivision:33460&in=state:24+county:033");
             apiRequest.Headers.Add("X-Census-Key", ConfigurationManager.AppSettings["X-Census-Key"]); // used to add keys.
             apiRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
             HttpWebResponse apiResponse = (HttpWebResponse)apiRequest.GetResponse();
@@ -33,13 +33,19 @@ namespace FinalProject.Controllers
             }
             return View();
         }
+        public ActionResult PresentInput()
+        {
+            HowsLifeEntities ORM = new HowsLifeEntities();
+            List<UserInput> infoList = ORM.UserInputs.ToList();
+            ViewBag.userInput = infoList;
+            return View("PresentInput");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
